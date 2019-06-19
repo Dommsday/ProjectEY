868c1017b1e2658c49db37d0c477a3bfe7ec2a02
unknown <Julienzimmermann@msn.com>
2019-06-19 18:20:12 +0200

    Fin du fonctionnement de la 1er enigme 'Trouver la bonne maison'

--- js/animation.js
+++ js/animation.js
@@ -1,15 +1,17 @@
-const indicationScreenOne = document.getElementById("picture_house");
 const pictureScreenOne = document.getElementById("container_indication");
+const buttonPicture = document.getElementById("button_picture");
 const buttonCross = document.getElementById("buttonCross");
 
 function screenOne(){
     
-    indicationScreenOne.addEventListener("click", () =>{
+    buttonPicture.addEventListener("click", () =>{
         TweenMax.to(pictureScreenOne, 2, {bottom: "0px"});
+        buttonCross.style.display="block";
     });
 
     buttonCross.addEventListener("click", () =>{
         TweenMax.to(pictureScreenOne, 1, {bottom: "-150px"});
+        buttonCross.style.display="none";
     }); 
 
     
