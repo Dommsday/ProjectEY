cda5f2e89f6d6f16c45a7307440ec44ef371b4b5
unknown <Julienzimmermann@msn.com>
2019-06-22 18:06:34 +0200

    Préparation de l'enigme VMC

---
+++ js/animationSecretRoom.js
@@ -0,0 +1,10 @@
+const curseur = document.getElementById("curseur");
+const arrayNumber = [1,2,3,4];
+
+function test(){
+
+    const p = document.createElement("p");
+    curseur.appendChild(p);
+}
+
+test();
